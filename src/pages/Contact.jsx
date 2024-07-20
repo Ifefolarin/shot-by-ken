0537789d2b5bed2c6f5f7f39e0489c35781112ea
Ife Folarin <babafolarin@Ifes-MacBook-Pro.local>
2024-07-19 20:34:50 -0400

    update contacts.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import ContactForm from "../features/contact/ContactForm";
 
 function Contact() {
   return (
-    <div className="flex lg:absolute lg:right-0 flex-col items-start justify-center w-full gap-4 px-2 py-2 md:py-4 lg:py-20  md:items-center  bg-white lg:w-[50rem] lg:mt-10 md:px-10 ">
+    <div className="flex lg:absolute lg:right-0 flex-col items-center justify-center w-full gap-4 px-2 py-2 md:py-4 lg:py-20  md:items-center  bg-white lg:w-[50rem] lg:mt-10 md:px-10 ">
       <ContactDetails />
       <ContactForm />
     </div>
